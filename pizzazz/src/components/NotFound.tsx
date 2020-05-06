d8afc722fac385097fc00681c31f9ba849227550
mathena828 <mathena828@gmail.com>
2020-05-06 20:50:04 +0800

    Updated NotFound Component

--- pizzazz/src/components/NotFound.tsx
+++ pizzazz/src/components/NotFound.tsx
@@ -2,7 +2,19 @@ import * as React from "react";
 
 class NotFound extends React.Component {
   render() {
-    return <div>PAGE NOT FOUND</div>;
+    return (
+      <div className="container mt-5 text-center">
+        <div
+          style={{
+            fontFamily: "Roboto Slab, serif",
+            fontWeight: "bold",
+            fontSize: "3em",
+          }}
+        >
+          Page Not Found
+        </div>
+      </div>
+    );
   }
 }
 
